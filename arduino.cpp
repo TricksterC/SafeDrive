629459604a2399e371ca860b2cd458e23bf70bd7
Srujan Raj <ramsrujanraj@gmail.com>
2025-04-06 21:20:59 +0530

    Update arduino.cpp
    made changes to initialization, fixed missing code

--- arduino.cpp
+++ arduino.cpp
@@ -1,17 +1,36 @@
-const int ledPin = 13;  // LED connected to pin 13
+#define Led1 12
+#define Led2 11
+#define Led3 10
+#define Led4 6
+#define Led5 7
+#define playsound 3
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(9600);  // Start Serial Communication at 9600 baud
+  pinMode(Led1, OUTPUT);
+  pinMode(Led2, OUTPUT);
+  pinMode(Led3, OUTPUT);
+  pinMode(Led4, OUTPUT);
+  pinMode(Led5, OUTPUT);
 }
 
 void loop() {
-    if (Serial.available() > 0) {  // Check if data is available
-        char command = Serial.read();  // Read the received character
-        if (command == '1') {
-            digitalWrite(ledPin, HIGH);  // Turn LED ON
-        } else if (command == '0') {
-            digitalWrite(ledPin, LOW);   // Turn LED OFF
-        }
-    }
+  // adjust on and off delay time acc to use case
+  digitalWrite(Led1, HIGH);
+  digitalWrite(Led2, HIGH);
+  digitalWrite(Led3, HIGH);
+  digitalWrite(Led4, HIGH);
+  digitalWrite(Led5, HIGH);
+  delay(5000); 
+  
+  digitalWrite(Led1, LOW);
+  digitalWrite(Led2, LOW);
+  digitalWrite(Led3, LOW);
+  digitalWrite(Led4, LOW);
+  digitalWrite(Led5, LOW);
+  delay(100); 
+
+  digitalWrite(playsound, HIGH);
+  delay(100);
+  digitalWrite(playsound, LOW);
+  delay(3000);
 }
