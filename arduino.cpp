cff3e66c8dc1f5e3d9876954441cfda93547e038
Srujan Raj <ramsrujanraj@gmail.com>
2025-04-08 22:40:11 +0530

    Update arduino.cpp
    used booleans to update value

--- arduino.cpp
+++ arduino.cpp
@@ -14,23 +14,45 @@ void setup() {
 }
 
 void loop() {
-  // adjust on and off delay time acc to use case
-  digitalWrite(Led1, HIGH);
-  digitalWrite(Led2, HIGH);
-  digitalWrite(Led3, HIGH);
-  digitalWrite(Led4, HIGH);
-  digitalWrite(Led5, HIGH);
-  delay(5000); 
-  
-  digitalWrite(Led1, LOW);
-  digitalWrite(Led2, LOW);
-  digitalWrite(Led3, LOW);
-  digitalWrite(Led4, LOW);
-  digitalWrite(Led5, LOW);
-  delay(100); 
 
-  digitalWrite(playsound, HIGH);
-  delay(100);
-  digitalWrite(playsound, LOW);
-  delay(3000);
+  if (Serial.available() > 0) {
+  String data = Serial.readStringUntil('\n');
+  int commaIndex = data.indexOf(',');
+  int shutEye = data.substring(0, commaIndex).toInt();
+  int yawn = data.substring(commaIndex + 1).toInt();
+  // adjust on and off delay time acc to use case
+
+  if(shutEye==1)
+  {
+    digitalWrite(Led1, HIGH);
+    digitalWrite(Led2, HIGH);
+    digitalWrite(Led3, HIGH);
+    digitalWrite(Led4, HIGH);
+    digitalWrite(Led5, HIGH);
+    delay(5000); 
+  
+    digitalWrite(Led1, LOW);
+    digitalWrite(Led2, LOW);
+    digitalWrite(Led3, LOW);
+    digitalWrite(Led4, LOW);
+    digitalWrite(Led5, LOW);
+    delay(100); 
+
+    digitalWrite(playsound, HIGH);
+    delay(100);
+    digitalWrite(playsound, LOW);
+    delay(3000);
+  }
+    
+    
+  
+
+  if(yawn==1)
+  {
+     digitalWrite(playsound, HIGH);
+     delay(100);
+     digitalWrite(playsound, LOW);
+     delay(3000);
+  } 
+ 
 }
